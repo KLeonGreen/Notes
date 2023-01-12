24bf17e4d6514979a7bf036cff8e8c0dc183e76b
KLeonGreen <114649634+KLeonGreen@users.noreply.github.com>
2023-01-12 15:50:56 +0100

    cloudinary

--- src/api/files/index.js
+++ src/api/files/index.js
@@ -1,14 +1,27 @@
 import express from "express";
 import multer from "multer";
 import { getPlanners, saveFile, writePlanners } from "../../library/fs-tools.js";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 
 const filesRouter = express.Router();
 
-filesRouter.post("/file", multer().single("file"), async (req, res, next) => {
+const cloudinaryUploader = multer({
+  storage: new CloudinaryStorage({
+    cloudinary,
+    params: { folder: "planners/files" },
+  }),
+}).single("file");
+
+filesRouter.post("/:id/file", cloudinaryUploader, async (req, res, next) => {
   try {
-    const fileName = req.file.originalname;
-    await saveFile(fileName, req.file.buffer);
-    const url = `localhost:5000/images/${fileName}`;
+    // const fileName = req.file.originalname;
+    // await saveFile(fileName, req.file.buffer);
+    // const url = `localhost:5000/images/${fileName}`;
+
+    console.log(req.file);
+
+    const url = req.file.path;
 
     const planners = await getPlanners();
     const planINdex = planners.findIndex((planner) => planner.id === req.params.id);
@@ -16,7 +29,7 @@ filesRouter.post("/file", multer().single("file"), async (req, res, next) => {
       const newPlanner = { ...planners[planINdex], file: url, updated: new Date() };
       planners[planINdex] = newPlanner;
 
-      await writePlanners();
+      await writePlanners(planners);
     }
 
     res.send({ Message: "File Added" });
